6d42ff0da0bd2b02691e208ed6164fbdef40e6da
Mike Messmore <mike@messmore.org>
2014-06-01 11:05:09 -0500

    Reworked logging

--- garden_buddy.py
+++ garden_buddy.py
@@ -2,41 +2,87 @@
 import time
 import os
 import sys
+import signal
+import argparse
+import logging
+
 sys.path.append("./lib")
-import moisture
+import analog
 import temperature
 import rrd
 
-import signal
-
-DEBUG=True
 
 def cleanup(signum, frame):
     sys.exit(0)
 
+def getopts():
+    description='Log garden sensor data to RRD'
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('-D', '--debug',
+            dest='debug',
+            action='store_true',
+            help="Run in debug mode (overrides -l, -o)")
+    parser.add_argument('-l', '--log-level',
+            dest='loglevel',
+            choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+            default='WARNING',
+            help="Set log level")
+    parser.add_argument('-o', '--logfile',
+            action='store',
+            dest='logfile',
+            default='-',
+            help="Set log file")
+    parser.add_argument('-n', '--no-write',
+            dest='no',
+            action='store_true',
+            help="Skip writing to RRD")
+    parser.add_argument('-s', '--interval',
+            dest='interval',
+            action='store',
+            type=int,
+            default=300,
+            help="Sleep Interval")
+    opts = parser.parse_args()
+
+    logging_format = '%(asctime)s %(levelname)s:%(message)s'
+    if opts.debug:
+        logging.basicConfig(format=logging_format, level=logging.DEBUG)
+        return opts
+
+    numeric_level = getattr(logging, opts.loglevel, None)
+    logging.basicConfig(format=logging_format, level=numeric_level)
+    if opts.logfile != "-":
+        fh = logging.handlers.WatchedFileHandler(opts.logfile)
+        logger = logging.getLogger(__name__)
+        logger.addHandler(fh)
+
+    return opts
+
 def main():
+
+    opts = getopts()
+    logger = logging.getLogger(__name__)
 
     signal.signal(signal.SIGTERM, cleanup)
     signal.signal(signal.SIGINT, cleanup)
 
 
     while True:
-        # read the analog pin
-        moisture_value = moisture.poll()
+        # read the analog pins
+        moisture_value = analog.poll(channel=0)
+        pot_value = analog.poll(channel=1)
 
         # poll the thermometer
         temperature_value = temperature.poll()
 
-        if DEBUG:
-            print "time:", time.strftime("%c")
-            print "moisture_value:", moisture_value
-            print "temperature_value:", temperature_value
-            print "------------------------------------"
+        logger.info("moisture_value:%s", moisture_value)
+        logger.info("pot_value:%s", pot_value)
+        logger.info("temperature_value:%s", temperature_value)
 
-        rrd.update(moisture_value, temperature_value)
+        if not opts.no:
+            rrd.update(moisture_value, temperature_value)
 
-        # hang out and do nothing for 5 secs
-        time.sleep(300)
+        time.sleep(opts.interval)
 
     # We should never get here, but just in case
     moisture.teardown()
--- lib/analog.py
+++ lib/analog.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 import sys
 import atexit
+import logging
 import RPi.GPIO as GPIO
-
-DEBUG = 0
 
 # change these as desired - they're the pins connected from the
 # SPI port on the ADC to the Cobbler
@@ -11,14 +10,18 @@ SPI_CLK = 18
 SPI_MISO = 23
 SPI_MOSI = 24
 SPI_CS = 25
-MOISTURE_ADC = 0
+
 
 def readadc(adcnum, clockpin, mosipin, misopin, cspin):
     """read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)"""
-    if ((adcnum > 7) or (adcnum < 0)):
-        return -1
-    GPIO.output(cspin, True)
+    logger = logging.getLogger(__name__)
+    logger.debug("Reading value from Analog Channel %s", adcnum)
 
+    if ((adcnum > 7) or (adcnum < 0)):
+        logger.error("Invalid Analog Channel")
+        return -1
+
+    GPIO.output(cspin, True)
     GPIO.output(clockpin, False)  # start clock low
     GPIO.output(cspin, False)     # bring CS low
 
@@ -50,6 +53,8 @@ def readadc(adcnum, clockpin, mosipin, misopin, cspin):
 
 def setup():
     """ Initial GPIO setup"""
+    logger = logging.getLogger(__name__)
+    logger.debug("Setting up GPIO")
     GPIO.setmode(GPIO.BCM)
 
     # set up the SPI interface pins
@@ -62,14 +67,17 @@ def setup():
 
 def teardown(signum, frame):
     """Signal handler"""
-    if DEBUG:
-        print "Tearing it down"
+    logger = logging.getLogger(__name__)
+    logger.debug("Cleaning up GPIO for exit")
     GPIO.cleanup()
     if signum > 0:
         sys.exit(0)
 
-def poll():
-    return readadc(MOISTURE_ADC, SPI_CLK, SPI_MOSI, SPI_MISO, SPI_CS)
+def poll(channel=0):
+    logger = logging.getLogger(__name__)
+    value = readadc(channel, SPI_CLK, SPI_MOSI, SPI_MISO, SPI_CS)
+    logger.debug("Channel %s returned %d", channel, value)
+    return value
 
 # Set up our GPIO fun
 setup()
