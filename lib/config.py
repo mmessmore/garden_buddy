a35aea8bcebf63ddaeb81050d7cae5e39003dfc3
Mike Messmore <mike@messmore.org>
2014-06-04 21:36:24 -0500

    Set up config file parsing, and finished up weather

---
+++ lib/config.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import ConfigParser
+
+class ConfigError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return self.value
+
+
+class Config:
+    def __init__(self):
+        conffiles = [
+                '/etc/gb.conf',
+                '/usr/local/etc/gb.conf',
+                os.path.expanduser('~/.gb.conf'),
+                './gb.conf']
+
+        self.cp = ConfigParser.SafeConfigParser()
+        self.cp.read(conffiles)
+
+        self.sensors = {}
+        self.graphs = {}
+
+        self.parse()
+        self.validate()
+
+    def parse(self):
+        for section in self.cp.sections():
+            # Stub out our graphs
+            if section[0:5] == "graph":
+                for name in [ "title", "sensors" ]:
+                    if not self.cp.has_option(section, name):
+                        raise ConfigError(
+                                "Section {} missing \"{}\"".format(section,
+                                    name))
+                self.graphs[section] = self.cp.get(section, "sensors").split(",")
+            # Assume we're a sensor
+            else:
+                for name in [ "title", "type", "unit", "id" ]:
+                    if not self.cp.has_option(section, name):
+                        raise ConfigError(
+                                "Section {} missing \"{}\"".format(section,
+                                    name))
+                self.sensors[section] = {}
+                for (name, value) in self.cp.items(section):
+                    self.sensors[section][name] = value
+
+    def validate(self):
+        for (name, value) in self.graphs.items():
+            for sensor in value:
+                if not sensor in self.sensors.keys():
+                    raise ConfigError(
+                            "Error in {}: No such Sensor {}".format(name,
+                                sensor))
+
+if __name__ == "__main__":
+    config = Config()
+    print repr(config.sensors)
+    print repr(config.graphs)
--- lib/weather.py
+++ lib/weather.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+"""
+This is a dumb wrapper around pywapi just for NOAA, since I don't care about
+weather.dumb and their stupid made up winter storm names.
+
+For long running processes it keeps you in the sweet spot in terms of polling.
+"""
 
 import sys
 import logging
@@ -10,6 +16,10 @@ import email.utils
 logger = logging.getLogger(__name__)
 
 class Weather:
+    """ Simple abstration for NOAA.
+    Args:
+        station: NOAA Station ID
+    """
     def __init__(self, station):
         self.station = station
         self.timestamp = datetime.datetime(1970, 1, 1)
@@ -17,6 +27,7 @@ class Weather:
         self.poll()
 
     def poll(self):
+        """Refetch weather data if needed (based on time)"""
         if (datetime.datetime.now() - self.timestamp ) < self.timeout:
             return True
         logger.debug("Fetching Weather from %s", self.station)
@@ -27,22 +38,47 @@ class Weather:
         return True
 
     def temperature(self):
+        """Get current temperature in C
+
+        Returns:
+            The temperature in degrees Celcius
+        """
         self.poll()
         return self.weather['temp_c']
 
     def humidity(self):
+        """Get current relative humidity
+
+        Returns:
+            The humidity in numbers
+        """
         self.poll()
         return self.weather['relative_humidity']
 
     def wind(self):
+        """Get current windspeed in MPH
+
+        Returns:
+            The wind speed in Miles/Hour
+        """
         self.poll()
         return self.weather['wind_mph']
 
     def pressure(self):
+        """Get current pressure in mb
+
+        Returns:
+            The pressure in Milibars
+        """
         self.poll()
         return self.weather['pressure_mb']
 
     def dewpoint(self):
+        """Get current dewpoint in C
+
+        Returns:
+            The dewpoint in degrees Celcius
+        """
         self.poll()
         return self.weather['dewpoint_c']
 
@@ -53,4 +89,3 @@ if __name__ == '__main__':
     print "wind:", w.wind()
     print "pressure:", w.pressure()
     print "dewpoint:", w.dewpoint()
-
