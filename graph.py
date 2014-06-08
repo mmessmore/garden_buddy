62bd75a2c952a948dc731c0192ef223a49b36b6e
Mike Messmore <mike@messmore.org>
2014-06-07 19:52:42 -0500

    Tweaked to listen on 80 if root, 8080 otherwise

--- graph.py
+++ graph.py
@@ -78,4 +78,7 @@ def graph(graph):
     return resp
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    port = 8080
+    if os.getuid() == 0:
+        port = 80
+    app.run(host='0.0.0.0', port=port)
